ef5e69c5657a07c5f5470b24cce532e9062f9506
Saurabh Bharti <saurabhbharti9@gmail.com>
2022-01-27 13:35:42 +0530

    buffer labs

--- labs/ch-11/labs-1/index.js
+++ labs/ch-11/labs-1/index.js
@@ -1,7 +1,6 @@
-'use strict'
-const assert = require('assert')
-const buffer = Buffer.allocUnsafe(1024)
+"use strict";
+const assert = require("assert");
+const buffer = Buffer.alloc(1024);
 
-for (const byte of buffer) assert.equal(byte, buffer[0])
-console.log('passed!')
-
+for (const byte of buffer) assert.equal(byte, buffer[0]);
+console.log("passed!");

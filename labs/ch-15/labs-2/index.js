d6bbf5ad1e63880e6a14504b47766dc838192244
Saurabh Bharti <saurabhbharti9@gmail.com>
2022-02-03 23:31:34 +0530

    child process labs passed

--- labs/ch-15/labs-2/index.js
+++ labs/ch-15/labs-2/index.js
@@ -1,9 +1,14 @@
-'use strict'
+"use strict";
 
-const { spawn } = require('child_process')
+const { spawn } = require("child_process");
 
-function exercise (command, args) {
-  return spawn(command, args)
+function exercise(command, args) {
+  const sp = spawn(command, args, {
+    stdio: ["pipe", "inherit", "pipe"],
+  });
+  sp.stderr.pipe(process.stdout);
+  sp.stdin.end();
+  return sp;
 }
 
-module.exports = exercise
+module.exports = exercise;
